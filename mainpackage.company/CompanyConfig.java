db7d9ee41d27dbd66f6d405abe14cc859c018b6c
mberkehanergun <140403112+mberkehanergun@users.noreply.github.com>
2023-08-04 16:32:47 +0300

    Add files via upload

---
+++ mainpackage.company/CompanyConfig.java
@@ -0,0 +1,19 @@
+package mainpackage.company;
+
+public class CompanyConfig {
+    private static int hourOfTask1 = 2;
+    private static int hourOfTask2 = 3;
+    private static int hourOfTask3 = 4;
+    
+	public static int getHourOfTask1() {
+		return hourOfTask1;
+	}
+
+	public static int getHourOfTask2() {
+		return hourOfTask2;
+	}
+
+	public static int getHourOfTask3() {
+		return hourOfTask3;
+	}
+}
